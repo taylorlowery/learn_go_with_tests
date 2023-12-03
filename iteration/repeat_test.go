006f6c5af4cd6b9c1299029fb1789dfbe1817f35
Taylor Lowery <taylorlowery07@gmail.com>
2023-12-02 21:16:52 -0800

    test iteration and add arrays_and_slices

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,11 +1,35 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 3)
+	fmt.Println(repeated)
+	// Output: aaa
+}
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
-	if repeated != expected {
-		t.Errorf("got '%s', want '%s'", repeated, expected)
+	t.Run("3 times", func(t *testing.T) {
+		repeated := Repeat("a", 3)
+		expected := "aaa"
+		if repeated != expected {
+			t.Errorf("got '%s', want '%s'", repeated, expected)
+		}
+	})
+	t.Run("0 times", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := ""
+		if repeated != expected {
+			t.Errorf("got '%s', want '%s'", repeated, expected)
+		}
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 3)
 	}
 }
